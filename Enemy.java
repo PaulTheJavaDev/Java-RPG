84db5aed3051ad6c02ec1d6848ce3fd7c5929a56
Paul-Ludwig <Paul-Ludwig.Simon@t-online.de>
2025-04-19 15:52:21 +0200

    Create Enemy.java

---
+++ Enemy.java
@@ -0,0 +1,14 @@
+public class Enemy {
+
+    String name;
+    int hp;
+    int damage;
+    int goldDrop;
+
+    Enemy(String name, int hp, int damage, int goldDrop) {
+        this.name = name;
+        this.hp = hp;
+        this.damage = damage;
+        this.goldDrop = goldDrop;
+    }
+}
